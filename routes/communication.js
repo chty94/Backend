0e5579ce1a8fa617043dcc8223da18b770e62024
Tae Yeon Chung <cau_chty94@naver.com>
2020-05-31 01:20:14 +0900

    createContent 수정

--- routes/communication.js
+++ routes/communication.js
@@ -18,7 +18,7 @@ router.get('/', async(req, res, next) => {
 router.post('/createContent', async(req, res, next) => {
   try {
     console.log('Create Content');
-    let results = await db.communicationinsert(req.body.gmail, req.body.title, req.body.content, req.body.date, req.body.time);
+    let results = await db.communicationinsert(req.body.name, req.body.title, req.body.content, req.body.date, req.body.time);
     console.log(req.body);
     if (results == "done"){
       res.header("Access-Control-Allow-Origin", "*");
