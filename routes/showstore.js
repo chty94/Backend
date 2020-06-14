977d22a4756479d900faf99ae39de79396e19d0b
Tae Yeon Chung <cau_chty94@naver.com>
2020-06-15 05:33:44 +0900

    showstore.js 라우팅 순서 바꿨음

--- routes/showstore.js
+++ routes/showstore.js
@@ -3,6 +3,27 @@ var db = require('../config/database');
 
 var router = express.Router();
 
+router.get('/totalpage', async(req, res, next) => {
+  try {
+    let results = await db.showstoretotalpage();
+    var totalPage = 0;
+    if (results[0].total === 0) {
+      totalPage = 1;
+    }
+    else {
+      totalPage = Math.ceil(results[0].total/5);
+    }
+    var result = { "total" : totalPage };
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(result);
+  } catch(e) {
+    console.log(e);
+    console.log('something happened in showstore/totalpage');
+    res.sendStatus(500);
+  }
+});
+
 router.get('/:page', async(req, res, next) => {
   try {
     var offset = (req.params.page-1) * 5;
@@ -13,27 +34,6 @@ router.get('/:page', async(req, res, next) => {
   } catch(e) {
     console.log(e);
     console.log('something happened in showstore.js');
-    res.sendStatus(500);
-  }
-});
-
-router.get('/totalpage', async(req, res, next) => {
-  try {
-    let results = await db.showstoretotalpage();
-    var totalPage = 0;
-    if (results[0].total === 0) {
-      totalPage = 1;
-    }
-    else {
-      totalPage = Math.ceil(results[0].total/10);
-    }
-    var result = { "total" : totalPage };
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.json(result);
-  } catch(e) {
-    console.log(e);
-    console.log('something happened in showstore/totalpage');
     res.sendStatus(500);
   }
 });
