15485e8798e85893f9944cbb6103d7d71a79570a
Tae Yeon Chung <cau_chty94@naver.com>
2020-06-15 03:32:04 +0900

    store/insert 와 store/update 최종이길...

--- routes/store.js
+++ routes/store.js
@@ -3,31 +3,11 @@ var db = require('../config/database');
 
 var router = express.Router();
 
-router.post('/', async(req, res, next) => {
-  try {
-    let results = await db.store(req.body.gmail);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.json(results);
-  } catch(e) {
-    console.log(e);
-    console.log('something happened in store.js');
-    res.sendStatus(500);
-  }
-});
-
 router.post('/insert', async(req, res, next) => {
     try {
-
-        //    var geocoder = new kakao.maps.services.Geocoder();
-        //    var callback = function(result, status) {
-        //        if (status === kakao.maps.services.Status.OK) {
-        //            console.log(results)
-        //        }
-        //    }
-
         var lat = parseFloat(req.body.latitude);
         var long = parseFloat(req.body.longitude);
-        let results = await db.storeinsert(req.body.gmail, req.body.name, req.body.newname, req.body.address, lat, long, req.body.category, req.body.phone, req.body.information);
+        let results = await db.storeinsert(req.body.gmail, req.body.name, req.body.address, lat, long, req.body.category, req.body.phone, req.body.information);
         if (results === "done") {
             res.header("Access-Control-Allow-Origin", "*");
             res.json(results);
@@ -43,24 +23,6 @@ router.post('/insert', async(req, res, next) => {
         res.sendStatus(500);
     }
 });
-
-router.post('/delete', async(req, res, next) => {
-    try {
-        let results = await db.storedelete(req.body.gmail, req.body.name);
-        if (results == "done") {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.json(results);   
-        }
-        else {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.send('x');   
-        }
-    } catch(e) {
-        console.log(e);
-        console.log('something happened in store/insert');
-        res.sendStatus(500);
-    }
-  });
 
 router.post('/update', async(req, res, next) => {
     try {
@@ -81,5 +43,38 @@ router.post('/update', async(req, res, next) => {
         res.sendStatus(500);
     }
   }); 
+
+router.post('/delete', async(req, res, next) => {
+try {
+    let results = await db.storedelete(req.body.gmail, req.body.name);
+    if (results == "done") {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.json(results);   
+    }
+    else {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send('x');   
+    }
+} catch(e) {
+    console.log(e);
+    console.log('something happened in store/insert');
+    res.sendStatus(500);
+}
+});
+
+
+router.post('/', async(req, res, next) => {
+  try {
+    let results = await db.store(req.body.gmail);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(results);
+  } catch(e) {
+    console.log(e);
+    console.log('something happened in store.js');
+    res.sendStatus(500);
+  }
+});
+
+
 
 module.exports = router;
